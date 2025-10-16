b4e9671e5edc191a9f669c9d9c58166ba91db940
momenibrahim180-svg <momenibrahim180@gmail.com>
2025-10-16 19:58:43 +0300

    - Added Screen util package. - Added AppColor & Added main color.

---
+++ lib/ecommerce_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Ecommerce App',
+          theme: ThemeData(
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: child,
+        );
+      },
+    );
+  }
+}
