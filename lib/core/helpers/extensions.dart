82509f8662affe0066932cd27d8cdf0235ead0ae
momenibrahim180-svg <momenibrahim180@gmail.com>
2025-10-19 01:09:36 +0300

    - Added Helper Folder & Added extension for sized box.

---
+++ lib/core/helpers/extensions.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+extension SizedBoxExtension on num {
+  SizedBox get height => SizedBox(height: toDouble().h);
+  SizedBox get width => SizedBox(width: toDouble().w);
+}
