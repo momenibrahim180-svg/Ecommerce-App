9e3080400852f1c2267b695a0d4683d30e1b8775
momenibrahim180-svg <momenibrahim180@gmail.com>
2025-12-20 15:19:27 +0200

    - Added assets , colors & Styles.

--- lib/core/theming/app_color.dart
+++ lib/core/theming/app_color.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 abstract class AppColor {
   static const Color mainColor = Color(0xFFF25912);
+  static const Color secondColor = Color(0xFFF8B08F);
   static const Color blackColor = Color(0xFF0C0C0C);
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color greyColor = Colors.grey;
+  static const Color redColor = Color(0xFFCF1010);
 }
--- lib/core/theming/app_text_style.dart
+++ lib/core/theming/app_text_style.dart
@@ -3,9 +3,49 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class AppTextStyle {
+  static TextStyle font12W400Grey = TextStyle(
+    fontSize: 12.0.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColor.greyColor,
+  );
   static TextStyle font12W400MainColor = TextStyle(
     fontSize: 12.0.sp,
     fontWeight: FontWeight.w400,
+    color: AppColor.mainColor,
+  );
+  static TextStyle font12W500White = TextStyle(
+    fontSize: 12.0.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColor.whiteColor,
+  );
+  static TextStyle font12W500 = TextStyle(
+    fontSize: 12.0.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColor.blackColor,
+  );
+  static TextStyle font12W500Grey = TextStyle(
+    fontSize: 12.0.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColor.greyColor,
+  );
+  static TextStyle font12W600Grey = TextStyle(
+    fontSize: 12.0.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColor.greyColor,
+  );
+  static TextStyle font12W600 = TextStyle(
+    fontSize: 12.0.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColor.blackColor,
+  );
+  static TextStyle font12W700 = TextStyle(
+    fontSize: 12.0.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColor.blackColor,
+  );
+  static TextStyle font12W700MainColor = TextStyle(
+    fontSize: 12.0.sp,
+    fontWeight: FontWeight.w500,
     color: AppColor.mainColor,
   );
   static TextStyle font13W500 = TextStyle(
@@ -13,15 +53,44 @@ abstract class AppTextStyle {
     fontWeight: FontWeight.w500,
     color: AppColor.greyColor,
   );
+
+  static TextStyle font14W500Grey = TextStyle(
+    fontSize: 14.0.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColor.greyColor,
+  );
+
+  static TextStyle font14W600Grey = TextStyle(
+    fontSize: 14.0.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColor.greyColor,
+  );
+
   static TextStyle font14W700 = TextStyle(
     fontSize: 14.0.sp,
     fontWeight: FontWeight.w700,
     color: AppColor.blackColor,
   );
 
+  static TextStyle font15W600 = TextStyle(
+    fontSize: 15.0.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColor.blackColor,
+  );
+
   static TextStyle font15W700 = TextStyle(
     fontSize: 15.0.sp,
     fontWeight: FontWeight.w700,
+    color: AppColor.blackColor,
+  );
+  static TextStyle font16W500 = TextStyle(
+    fontSize: 16.0.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColor.blackColor,
+  );
+  static TextStyle font16W600 = TextStyle(
+    fontSize: 16.0.sp,
+    fontWeight: FontWeight.w600,
     color: AppColor.blackColor,
   );
   static TextStyle font16W700Black = TextStyle(
@@ -34,9 +103,24 @@ abstract class AppTextStyle {
     fontWeight: FontWeight.w700,
     color: AppColor.whiteColor,
   );
+  static TextStyle font16W700MainColor = TextStyle(
+    fontSize: 16.0.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColor.mainColor,
+  );
+  static TextStyle font20W600 = TextStyle(
+    fontSize: 20.0.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColor.blackColor,
+  );
   static TextStyle font20W700 = TextStyle(
     fontSize: 20.0.sp,
     fontWeight: FontWeight.w700,
+    color: AppColor.blackColor,
+  );
+  static TextStyle font24W600 = TextStyle(
+    fontSize: 24.0.sp,
+    fontWeight: FontWeight.w600,
     color: AppColor.blackColor,
   );
   static TextStyle font24W700 = TextStyle(
